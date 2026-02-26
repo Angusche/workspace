c1e75052a6a29528e7cfed73e6f4e6f17acdd1c3
Angus Chen <angus.chen@intel.com>
2026-02-26 16:59:50 +0800

    Add environment setup and script to launch gmsl camera

---
+++ camera/scripts/gmsl/launch_arl_1_camera_dma.sh
@@ -0,0 +1,1 @@
+gst-launch-1.0 icamerasrc num-buffers=6000 scene-mode=auto device-name=isx031-1 io-mode=dma_mode printfps=true ! 'video/x-raw(memory:DMABuf),drm-format=UYVY,width=1920,height=1536' ! glimagesink sync=false
